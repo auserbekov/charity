8e1c3e3f866dbd55f2c57cbf8eb5ef8904b48833
auserbekov <a.userbekov@bk.ru>
2019-09-21 15:25:00 +0600

    Success and failure pages added: ver 2

--- resources/views/frontend/pages/success.blade.php
+++ resources/views/frontend/pages/success.blade.php
@@ -3,6 +3,25 @@
 
 @section('content')
 
-	<p>Success. Thank you for your donation!</p>
+	<div class="hero-wrap" style="background-image: url('images/bg_6.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+          <div class="col-md-7 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
+             <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="/">Home</a></span> <span>Success</span></p>
+            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Thank you!</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <section class="ftco-section bg-light">
+      	<div class="container">
+			<p>Success. Thank you for your donation!</p>
+      	</div>
+    </section>
+
+
 
 @endsection
